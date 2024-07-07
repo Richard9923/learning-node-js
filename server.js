8142e167fba6113fff0a9c2a7f72e8eafdc4f182
Richard <programmingislife23@gmail.com>
2024-07-07 11:35:20 -0300

    instalando nodemon

---
+++ server.js
@@ -0,0 +1,14 @@
+import http from 'http';
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+
+    res.writeHead(500, {'Content-Type': 'application.json'})
+    res.end(JSON.stringify({ message: 'Server Error'}));
+})
+
+server.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+} )
