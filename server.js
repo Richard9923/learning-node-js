84b22fde87f3f253b292ad80b9e16eea75a4c441
Richard <programmingislife23@gmail.com>
2024-07-07 15:28:50 -0300

    aprendendo a criar api

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 import http from 'http';
-const PORT = 8000;
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
 
-    res.writeHead(200, {'Content-Type': 'text-html'})
-    res.end('<h1>Hello World!</h1>')
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async(req, res) => {
+
+    try {
+        if (req.method == 'GET') {
+            let filePath;
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if (req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {
+                throw new Error('Not Found')
+            }
+
+           const data = await  fs.readFile(filePath)
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error ('Method not allowed');
+        }
+    } catch(error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+
+    }
 })
 
 server.listen(PORT, () => {
